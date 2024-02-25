b38bbdbf6f263c0f69f6234a1c1277ad2ef44cea
suyash-thakur <500067151@stu.upes.ac.in>
2024-02-26 00:52:28 +0530

    remove console

--- models/Image.js
+++ models/Image.js
@@ -44,7 +44,6 @@ const filterActive = function (next) {
 //before saving the image, create uid
 imageSchema.pre('save', async function (next) {
     const image = this;
-    console.log(image);
     if (!image.uid) {
         image.uid = await uid();
     }
