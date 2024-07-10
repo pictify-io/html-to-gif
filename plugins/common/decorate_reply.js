8f52b3c35945996fadc24009708ae5dfb29abb42
suyash-thakur <500067151@stu.upes.ac.in>
2024-07-11 03:05:02 +0530

    handle secure field according to env

--- plugins/common/decorate_reply.js
+++ plugins/common/decorate_reply.js
@@ -6,7 +6,8 @@ const uid = require('../../util/uid');
 const cookieOptions = {
     domain: process.env.FRONTEND_HOST,
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'development' ? false : true,
+    path: '/'
 }
 
 
