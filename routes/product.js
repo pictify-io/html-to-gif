c6c5efa5768aa9a7ff5ac20dede0475aa0b39ddc
suyash-thakur <500067151@stu.upes.ac.in>
2024-07-29 01:55:27 +0530

    added pricing

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,55 @@
 const { lemonSqueezySetup, listProducts } = require('@lemonsqueezy/lemonsqueezy.js');
 
 
+// **Free**: 50 requests per month - Starter
+
+// **$14**: 1,500 requests per month - Basic
+
+// **$29**: 3,500 requests per month - Standard
+
+// **$49**: 7,500 requests per month - **Professional**
+
+// **$69**: 12,000 requests per month - **Advanced**
+
+// **$99**: 20,000 requests per month - **Pro Plus**
+
+// **$149**: 40,000 requests per month - Business
+
+// **$199**: 60,000 requests per month - **Business Plus**
+
+// **$249**: 80,000 requests per month - **Premium**
+
+// **$349**: 120,000 requests per month - **Premium Plus**
+
+// **$449**: 150,000 requests per month - **Enterprise**
+
+// **$649**: 250,000 requests per month - **Enterprise Plus**
+
+// **$899**: 400,000 requests per month - Elite
+
+// **$1,299**: 600,000 requests per month - Elite Plus
+
+//   ** $1, 799 **: 1,000,000 requests per month - Ultimate
+
+const slugRequestPerMonthMap = {
+  'starter': 50,
+  'basic': 1500,
+  'standard': 3500,
+  'professional': 7500,
+  'advanced': 12000,
+  'pro-plus': 20000,
+  'business': 40000,
+  'business-plus': 60000,
+  'premium': 80000,
+  'premium-plus': 120000,
+  'enterprise': 150000,
+  'enterprise-plus': 250000,
+  'elite': 400000,
+  'elite-plus': 600000,
+  'ultimate': 1000000,
+}
+
+
 lemonSqueezySetup({
   apiKey: process.env.LEMONSQUEEZY_API_KEY,
   onError: (error) => {
@@ -11,11 +60,23 @@ lemonSqueezySetup({
 
 
 const getProducts = async (request, reply) => {
-  const products = await listProducts({
+  let products = await listProducts({
     page: { number: 1, size: 20 },
     filter: { storeId: 110208 }
   });
-  return products.data;
+
+  products = products.data.data.map((product) => {
+    return {
+      id: product.attributes.id,
+      name: product.attributes.name,
+      price: product.attributes.price,
+      price_formatted: product.attributes.price_formatted,
+      purchase_url: product.attributes.buy_now_url,
+      request_per_month: slugRequestPerMonthMap[product.attributes.slug],
+    }
+  }).sort((a, b) => a.price - b.price);
+
+  return { success: true, data: products };
 };
 
 
