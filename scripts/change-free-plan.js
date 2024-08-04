ab8b1ea535e6494c52708fcd8416ddba00a2aca5
suyash-thakur <500067151@stu.upes.ac.in>
2024-08-04 23:33:32 +0530

    change free plan

---
+++ scripts/change-free-plan.js
@@ -0,0 +1,14 @@
+const db = require('../db');
+const User = require('../models/User');
+
+const changeFreePlan = async () => {
+  await User.updateMany({ currentPlan: 'free' }, { currentPlan: 'starter' });
+}
+
+const main = async () => {
+  await db();
+  await changeFreePlan();
+  process.exit(0);
+}
+
+main();
