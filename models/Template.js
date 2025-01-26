dd2ef15f2ed770838b179f4e2827fb91f3c6d0da
suyash-thakur <500067151@stu.upes.ac.in>
2025-01-27 02:32:32 +0530

    add og image APIs

--- models/Template.js
+++ models/Template.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const uid = require('../util/uid')
+const cheerio = require('cheerio');
 
 const templateSchema = new mongoose.Schema({
   uid: {
@@ -66,6 +67,36 @@ templateSchema.methods.populateTemplate = async function (variables) {
     html = html.replaceAll(`{{${key}}}`, value)
   })
   return html
+}
+
+templateSchema.methods.populateOgImage = async function (variables) {
+  if (this.type !== 'og-image') {
+    throw new Error('Template is not an og-image')
+  }
+  let html = this.html;
+  const $ = cheerio.load(html);
+
+  const validVariables = ['heading', 'description', 'logo'];
+  validVariables.forEach((key) => {
+    const value = variables[key];
+    if (!value) {
+      return;
+    }
+
+    switch (key) {
+      case 'heading':
+        $('#template-heading').text(value);
+        break;
+      case 'description':
+        $('#template-subheading').text(value);
+        break;
+      case 'logo':
+        $('#template-logo').attr('src', value);
+        break;
+    }
+  });
+
+  return $.html();
 }
 
 const filterActive = function (next) {
